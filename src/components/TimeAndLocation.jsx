32994d473927451114ea44ee2288a201acdf1107
Raviraj2022 <ravirajsahu312@gmail.com>
2024-04-09 16:12:12 +0530

    completed time and location component..

---
+++ src/components/TimeAndLocation.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const TimeAndLocation = () => {
+  return (
+    <div>
+      <div className="flex items-center justify-center my-6">
+        <p className="text-white text-xl font-extralight">
+          Tuesday, 31 May 2024 | Local time:16:40 PM
+        </p>
+      </div>
+      <div className="flex items-center justify-center my-3">
+        <p className="text-white text-xl font-meduim">
+          Berlin, DE
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default TimeAndLocation;
