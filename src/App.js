61497952b57c9535a0bef889769215f19e97c145
Raviraj2022 <ravirajsahu312@gmail.com>
2024-04-09 17:33:37 +0530

    complete temp and details design is done.

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import UilReact from "@iconscout/react-unicons/icons/uil-react";
 import TopButton from "./components/TopButton";
 import Input from "./components/Input";
 import TimeAndLocation from "./components/TimeAndLocation";
+import TemperatureAndDetails from "./components/TemperatureAndDetails";
 function App() {
   return (
     <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-500">
       <TopButton />
       <Input />
       <TimeAndLocation />
+      <TemperatureAndDetails />
     </div>
   );
 }
